6cb04adac6497d7010e6d2bc1337dd94532009fe
Mike Cho <elegantspanishguitar@gmail.com>
2021-10-10 09:33:30 -0700

    second commit

--- index.php
+++ index.php
@@ -1,18 +1,40 @@
+<?php
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
 
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	
-	<title>Book Reviews</title>
-	
-	<link rel="stylesheet" href="assets/styles/main.css">
-	
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    
+    <title>Book Reviews</title>
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
 </head>
 <body>
-
-	<script src="assets/scripts/main.js"></script>
-
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+   <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" 
+         data-target="#menu">
+         <span class="sr-only">Toggle navigation</span>
+         <span class="icon-bar"></span>
+         <span class="icon-bar"></span>
+         <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.php">Silverado</a>
+   </div>
+   <div class="collapse navbar-collapse" id="menu">
+      <ul class="nav navbar-nav">
+         <li><a href="index.php">Home</a></li>
+         <li><a href="movies.php">Movies</a></li>
+         <li class="active"><a href="price.php">Price List</a></li>
+         <li><a href="contact.php">Contact Us</a></li>
+      </ul>
+   </div>
+</nav>
+ 
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script type="text/javascript" src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
 </body>
 </html>
